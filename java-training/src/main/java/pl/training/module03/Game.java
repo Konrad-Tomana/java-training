aa9b328c47a689032ba728740ad0323770bb7071
tomana.konrad@gmail.com <sFJbdc@2RuxRiDL>
2025-05-12 16:04:52 +0200

    TicTacToe.java - make move steps

--- java-training/src/main/java/pl/training/module03/Game.java
+++ java-training/src/main/java/pl/training/module03/Game.java
@@ -4,6 +4,8 @@ public class Game {
 
     public static void main(String[] args) {
         var ticTacToe = new TicTacToe();
+        ticTacToe.makeMove(0,0);
+        ticTacToe.makeMove(0,1);
         ticTacToe.printBoard();
 
     }
--- java-training/src/main/java/pl/training/module03/TicTacToe.java
+++ java-training/src/main/java/pl/training/module03/TicTacToe.java
@@ -1,5 +1,7 @@
 package pl.training.module03;
 
+import java.util.Arrays;
+
 public class TicTacToe {
 
     private static final byte EMPTY = 0;
@@ -7,6 +9,7 @@ public class TicTacToe {
     private static final byte PLAYER_O = 2;
 
     private final byte[] board = new byte[9];
+    private byte currentPlayer = PLAYER_X;
 
     public void printBoard() {
         for (int fieldIndex = 0; fieldIndex < board.length; fieldIndex++) {
@@ -21,9 +24,42 @@ public class TicTacToe {
                     System.out.print("O  ");
                     break;
             }
-            if ((fieldIndex + 1) % 3 == 0)
-                System.out.println();
+            if ((fieldIndex + 1) % 3 == 0) {
+                System.out.println("");
+            }
         }
+    }
+
+    public boolean makeMove(int column, int row) {
+        var fieldIndex = row * 3 + column;
+        if (!isOnBoard(fieldIndex) || !isFree(fieldIndex)) {
+            return false;
+        }
+        board[fieldIndex] = currentPlayer;
+        togglePlayer();
+        return true;
+
+    }
+
+    private boolean isOnBoard(int fieldIndex) {
+        return fieldIndex >= 0 && fieldIndex < board.length;
+    }
+
+    private boolean isFree(int fieldIndex) {
+        return board[fieldIndex] == EMPTY;
+    }
+
+    private void togglePlayer() {
+        if (currentPlayer == PLAYER_X){
+            currentPlayer = PLAYER_O;
+        } else {
+            currentPlayer = PLAYER_X;
+        }
+    }
+
+    public void reset() {
+        Arrays.fill(board, EMPTY);
+        currentPlayer = PLAYER_X;
     }
 
 }
