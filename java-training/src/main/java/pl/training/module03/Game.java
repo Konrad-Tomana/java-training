877f3ff8ae5e7d07967f1e9d04822cf750c11d54
tomana.konrad@gmail.com <sFJbdc@2RuxRiDL>
2025-05-12 16:53:56 +0200

    TicTacToe.java - get Winner, has player won

--- java-training/src/main/java/pl/training/module03/Game.java
+++ java-training/src/main/java/pl/training/module03/Game.java
@@ -6,7 +6,17 @@ public class Game {
         var ticTacToe = new TicTacToe();
         ticTacToe.makeMove(0,0);
         ticTacToe.makeMove(0,1);
+
+        ticTacToe.makeMove(1,0);
+        ticTacToe.makeMove(0,2);
+
+        ticTacToe.makeMove(2,0);
         ticTacToe.printBoard();
 
+        System.out.println("Is game over: " + ticTacToe.isGameOver());
+        System.out.println("Result: " + ticTacToe.getWinner());
+
+        ticTacToe.reset();
+        ticTacToe.printBoard();
     }
 }
--- java-training/src/main/java/pl/training/module03/TicTacToe.java
+++ java-training/src/main/java/pl/training/module03/TicTacToe.java
@@ -8,8 +8,21 @@ public class TicTacToe {
     private static final byte PLAYER_X = 1;
     private static final byte PLAYER_O = 2;
 
+    private static final int[][] winningSequences = {
+            {0, 1, 2}, // top row
+            {3, 4, 5}, // middle row
+            {6, 7, 8}, // bottom row
+            {0, 3, 6}, // left column
+            {1, 4, 7}, // middle column
+            {2, 5, 8}, // right column
+            {0, 4, 8}, // main diagonal
+            {2, 4, 6}  // anti-diagonal
+    };
+
     private final byte[] board = new byte[9];
     private byte currentPlayer = PLAYER_X;
+    private byte winner = EMPTY;
+    private boolean isGameOver = false;
 
     public void printBoard() {
         for (int fieldIndex = 0; fieldIndex < board.length; fieldIndex++) {
@@ -25,20 +38,26 @@ public class TicTacToe {
                     break;
             }
             if ((fieldIndex + 1) % 3 == 0) {
-                System.out.println("");
+                System.out.println();
             }
         }
     }
 
     public boolean makeMove(int column, int row) {
         var fieldIndex = row * 3 + column;
-        if (!isOnBoard(fieldIndex) || !isFree(fieldIndex)) {
+        if (isGameOver || !isOnBoard(fieldIndex) || !isFree(fieldIndex)) {
             return false;
         }
         board[fieldIndex] = currentPlayer;
-        togglePlayer();
+        if (hasPlayerWon()) {
+            winner = currentPlayer;
+            isGameOver = true;
+        } else if (isBoardFull()) {
+            isGameOver = true;
+        } else {
+            togglePlayer();
+        }
         return true;
-
     }
 
     private boolean isOnBoard(int fieldIndex) {
@@ -50,16 +69,49 @@ public class TicTacToe {
     }
 
     private void togglePlayer() {
-        if (currentPlayer == PLAYER_X){
+        if (currentPlayer == PLAYER_X) {
             currentPlayer = PLAYER_O;
         } else {
             currentPlayer = PLAYER_X;
         }
     }
 
+    private boolean isBoardFull() {
+        for (byte field : board) {
+            if (field == EMPTY) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasPlayerWon() {
+        for (int[] winningSequence : winningSequences) {
+            if (board[winningSequence[0]] == currentPlayer
+                    && board[winningSequence[1]] == currentPlayer
+                    && board[winningSequence[2]] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void reset() {
         Arrays.fill(board, EMPTY);
         currentPlayer = PLAYER_X;
+        winner = EMPTY;
+        isGameOver = false;
     }
 
+    public String getWinner() {
+        return switch (winner) {
+            case PLAYER_X -> "X wins";
+            case PLAYER_O -> "O wins";
+            default -> "Draw";
+        };
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
 }
